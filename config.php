e4794cba08e9ec3c7c0c88b8c92e6905c6f68643
mdmoinulhossain <mmoinulh100@gmail.com>
2022-02-10 00:08:31 +0600

    Data insert Done

--- config.php
+++ config.php
@@ -2,10 +2,8 @@
 
 $connect_db = mysqli_connect("localhost", "root", "", "crudphp");
 
-if($connect_db){
-    echo "database connect successfully";
-} else {
-    echo "Error From Database";
+if(!$connect_db){
+    echo "Connection Error From Database";
 }
 
 ?>
--- insert.php
+++ insert.php
@@ -1,6 +1,16 @@
 <?php 
 
+include 'config.php';
 
+if(isset($_POST['done'])){
+
+  $em = $_POST['email'];
+  $pw = $_POST['password'];
+
+  $insert = "INSERT INTO `userlist`(`email`, `password`) VALUES ('$em', '$pw')";
+
+  $insertQuery = mysqli_query($connect_db, $insert);
+}
 
 ?>
 
@@ -17,22 +27,21 @@
 </head>
 <body>
     
-<form class="container my-5">
+<form class="container my-5" method="POST">
     <p>Insert Data</p>
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+    <input type="email" class="form-control" name="email" id="exampleInputEmail1" aria-describedby="emailHelp">
   </div>
   <div class="mb-3">
     <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
+    <input type="password" class="form-control" id="exampleInputPassword1" name="password">
   </div>
   <div class="mb-3 form-check">
     <input type="checkbox" class="form-check-input" id="exampleCheck1">
     <label class="form-check-label" for="exampleCheck1">Check me out</label>
   </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="submit" class="btn btn-primary" name="done">Submit</button>
 </form>
 
 
