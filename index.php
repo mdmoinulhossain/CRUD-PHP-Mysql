0320b953e881b042201411102ed2a541858b4ab4
mdmoinulhossain <mmoinulh100@gmail.com>
2022-02-10 00:41:40 +0600

    Data Read/Display Completed

--- index.php
+++ index.php
@@ -1,5 +1,54 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD</title>
 
-echo "Hello World";
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+
+<div class="container">
+    <p>User List</p>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Username/Email</th>
+      <th scope="col">Password</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+  <?php
+include 'config.php';
+
+$display = "SELECT * FROM `userlist`";
+
+$displayQuery = mysqli_query($connect_db, $display);
+
+while($record = mysqli_fetch_array($displayQuery)){
+
 
 ?>
+    <tr>
+      <th scope="row"><?php echo $record['id']; ?></th>
+      <td><?php echo $record['email']; ?></td>
+      <td><?php echo $record['password']; ?></td>
+      <td><button type="button" class="btn btn-outline-success">Edit</button>
+<button type="button" class="btn btn-outline-danger">Delete</button></td>
+    </tr>
+    <?php } ?>
+   
+  </tbody>
+</table>
+<button type="button" class="btn btn-outline-primary"><a href="insert.php">Insert User? </a></button>
+</div>
+    
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
--- insert.php
+++ insert.php
@@ -20,7 +20,7 @@ if(isset($_POST['done'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CRUD</title>
+    <title>Insert</title>
 
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
@@ -43,6 +43,9 @@ if(isset($_POST['done'])){
   </div>
   <button type="submit" class="btn btn-primary" name="done">Submit</button>
 </form>
+<div class="container">
+<button type="button" class="btn btn-outline-info"><a href="index.php"> View List?</a></button>
+</div>
 
 
 
