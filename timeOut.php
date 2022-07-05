8966c67ccf10eb677f6965ff9e927ea63bbd3784
mdmoinulhossain <mdmoinulhossain100@gmail.com>
2022-07-05 13:49:40 +0600

    date matchs

--- timeOut.php
+++ timeOut.php
@@ -5,15 +5,17 @@
 <?php
 // die();
 date_default_timezone_set('Asia/Dhaka');
-echo date('h:i:s'). "<br/>";
+echo date('Y-m-d:::h:i:s'). "<br/><br/><br/>";
 
 //sleep for 3 seconds / code execute after 3 seconds
 // sleep(3);
+$fixedDate = '11:40:00';
+$currDate = date('H:i:s');
+echo $fixedDate.' and '.$currDate.'<br/>';
 
-if(date('11:40:00') > date('h:i:s')){
-//start again
+if($fixedDate > $currDate){
 echo "hellow Bangladesh";
-} elseif (date('11:40:00') < date('h:i:s')) {
+} elseif($fixedDate < $currDate) {
 	// die();
     echo "Goodbye Bangladesh";
 }
